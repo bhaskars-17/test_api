89e508bb2943379b435d9fa676b7c0aa5b8f31cd
bhaskars-17 <bhaskars17812@gmail.com>
2026-04-06 11:26:35 +0530

    Fix formatting of success status message

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def get_posts():
     response = requests.request("GET", url, headers=headers, data=payload)
     return jsonify({
         "data": response.json(),
-        "status": "successfully ",
+        "status": "successfully",
         "status_code": 200
     })
 
